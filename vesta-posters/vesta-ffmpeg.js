830a6fb66448e5e4f7dbc7b7d8d673ef7bb30cd5
EduardoG1024 <spawnlol2424@gmail.com>
2026-05-22 23:37:09 -0700

    ffmpeg fix

--- vesta-posters/vesta-ffmpeg.js
+++ vesta-posters/vesta-ffmpeg.js
@@ -6,9 +6,6 @@ import fs from 'fs';
 const __dirname = import.meta.dirname;
 const postersPath = '../posters';
 
-const testName = 'Vesta-BBC Hace que un Conejito De Nieve Comience a Derretirse(1080P).mp4';
-const testPath = '../uploads/Vesta-BBC Hace que un Conejito De Nieve Comience a Derretirse(1080P).mp4'
-
 // * SOLO RECIBIR MIMETYPE DE .MP4
 // ? videoPath DEBE DE SER => "../uploads/${Nombre-del-Video.mp4}"
 export const generatePosterVesta = (videoPath, ruta) => {
@@ -30,32 +27,3 @@ export const generatePosterVesta = (videoPath, ruta) => {
             // console.error(`Ocurrió un Error con: ${videoPath}, `, err);
         });
 }
-// generatePosterVesta(testPath)
-// const files = fs.readdirSync('../uploads')
-// // console.log(files);
-// const videos = [];
-// const postersPath = '../posters';
-
-// files.forEach(file => {
-//     if (file.endsWith('.mp4')) {
-//         const pathFile = `../uploads/${file}`;
-//         videos.push(pathFile);
-
-//         const fileNamePoster = `Poster-${file}.webp`
-
-//         ffmpeg(pathFile)
-//             .screenshots({
-//                 timestamps: [10],
-//                 filename: fileNamePoster,
-//                 folder: path.join(__dirname, postersPath)
-//             })
-//             .on('end', () => {
-//                 console.log(`File ${file} guardo su frame`);
-//             })
-//             .on('error', (err) => {
-//                 console.error('Error:', err);
-//             });
-
-//     }
-// });
-// console.log(videos);
